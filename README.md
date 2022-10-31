ba3e19775c5150f4074ec93aa395c6a1271c9a15
Marlineck <90250217+Marlineck@users.noreply.github.com>
2022-10-31 17:24:30 +0100

    Adapted example of websocket demo (#2)

--- README.md
+++ README.md
@@ -3,5 +3,18 @@ This is an experimental DDEV + websocket demo from https://stackoverflow.com/que
 
 To use:
 * Check it out
-* `ddev start`
-* Use it via wss://ddev-websocket-demo:3001 
+* `ddev start` (websocket server starts automatically)
+* call https://ddev-websocket-demo.ddev.site 
+
+**Example Javascript call on browsers console:**
+>var conn = new WebSocket('wss://ddev-websocket-demo:3001');
+  conn.onopen = function(e) {
+  console.log("Connection established!");
+  };
+conn.onmessage = function(e) {
+console.log(e.data);
+};
+
+You will see the console message "Connection established!" when the websocket connection in successful. Check also your ddev terminal and see the connection message here.
+You can start sending messages to other connected browsers:
+>conn.send('Hello World!');
--- bin/chat-server.php
+++ bin/chat-server.php
@@ -1,41 +1,18 @@
 <?php
-namespace MyApp;
 use Ratchet\Server\IoServer;
-use MyApp\Chat;
-
-use Ratchet\MessageComponentInterface;
-use Ratchet\ConnectionInterface;
-require dirname(__DIR__) . '/vendor/autoload.php';
-
-
-class Chat implements MessageComponentInterface {
-    public function onOpen(ConnectionInterface $conn) {
-    }
-
-    public function onMessage(ConnectionInterface $from, $msg) {
-    }
-
-    public function onClose(ConnectionInterface $conn) {
-    }
-
-    public function onError(ConnectionInterface $conn, \Exception $e) {
-    }
-}
-
-
-
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
+use MyApp\Chat;
 
-    require dirname(__DIR__) . '/vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
-    $server = IoServer::factory(
-        new HttpServer(
-            new WsServer(
-                new Chat()
-            )
-        ),
-        8080
-    );
+$server = IoServer::factory(
+    new HttpServer(
+        new WsServer(
+            new Chat()
+        )
+    ),
+    8080
+);
 
-    $server->run();
+$server->run();
